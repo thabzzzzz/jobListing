3640f5fbdca4e25aa1d7d76917750b025c3dae05
Thabiso <tmosenohi99@gmail.com>
2024-12-04 09:59:49 +0200

    Enabled auto format on save + react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
