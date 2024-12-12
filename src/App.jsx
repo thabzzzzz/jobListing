5c83530451dfc2b09bd56b42bf4d6364354f2a73
thabiso <tmosenohi99@gmail.com>
2024-12-12 09:58:38 +0200

    Added CSS + conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import { useEffect, useState } from "react";
-import { useMount } from "react-use";
-
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
 
-  useMount(async () => {
-    const response = await fetch("/data.json");
-    const result = await response.json();
-    setData(result);
-  });
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/data.json");
+      const result = await response.json();
+      setData(result);
+    };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch("/data.json");
-  //     const result = await response.json();
-  //     setData(result);
-  //   };
-
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <ul>
-        {data.map((job) => (
-          <li key={job.id}>{job.company}</li>
-        ))}
-      </ul>
-    </>
+    <div className="job-listings">
+      {data.map((job) => (
+        <div key={job.id} className={`job-card`}>
+          <div className="job-header">
+            <img src={job.logo} className="company-logo" />
+            <div>
+              <div className="company-name">
+                {job.company}
+                {job.new && <span className="badge new"> new</span>}
+                {job.featured && (
+                  <span className="badge featured"> features</span>
+                )}
+              </div>
+              <h2 className="job-title">{job.position}</h2>
+              <div className="job-meta">
+                <span>{job.postedAt}</span>
+                <span>• {job.contract}</span>
+                <span>• {job.location}</span>
+              </div>
+            </div>
+          </div>
+          <div className="job-tags">
+            {[job.role, job.level, ...job.languages, ...job.tools].map(
+              (value) => (
+                <span key={value} className="job-tag">
+                  {value}
+                </span>
+              )
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
